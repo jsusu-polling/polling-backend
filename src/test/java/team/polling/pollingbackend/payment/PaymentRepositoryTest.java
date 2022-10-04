a9ae08717f5b31480397b2bb075bf2f6dacb46f0
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 16:33:45 +0900

    test : 기본 Payment 테스트 환경 생성

---
+++ src/test/java/team/polling/pollingbackend/payment/PaymentRepositoryTest.java
@@ -0,0 +1,7 @@
+package team.polling.pollingbackend.payment;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class PaymentRepositoryTest {
+}
---
+++ src/test/java/team/polling/pollingbackend/payment/PaymentServiceTest.java
@@ -0,0 +1,4 @@
+package team.polling.pollingbackend.payment;
+
+public class PaymentServiceTest {
+}
