33f277fbfe335fddcf77960e61714360deb356fe
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 16:54:42 +0900

    test : SurveyServiceTest 로직 뼈대 작성

--- src/test/java/team/polling/pollingbackend/survey/SurveyServiceTest.java
+++ src/test/java/team/polling/pollingbackend/survey/SurveyServiceTest.java
@@ -1,4 +1,83 @@
 package team.polling.pollingbackend.survey;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import team.polling.pollingbackend.survey.service.QuestionService;
+import team.polling.pollingbackend.survey.service.SurveyService;
+
 public class SurveyServiceTest {
+
+    SurveyService surveyService;
+    QuestionService questionService;
+
+    @BeforeAll
+    void 기본_설문조사_개체들_생성() {
+
+    }
+
+    @Test
+    void 생성한_설문조사_리스트_가져오기() {
+
+    }
+
+    @Test
+    void 참여한_설문조사_리스트_가져오기() {
+
+    }
+
+    @Test
+    void 개별_설문조사_가져오기() {
+
+    }
+
+    @Test
+    void 설문조사_생성하기() {
+
+    }
+
+    @Test
+    void 설문조사_제목_생성_및_업데이트() {
+
+    }
+
+    @Test
+    void 설문조사_카테고리_생성_및_업데이트() {
+
+    }
+
+    @Test
+    void 설문조사_설명_생성_및_업데이트() {
+
+    }
+
+    @Test
+    void 설문조사_보상_입력하기() {
+
+    }
+
+    @Test
+    void 설문조사_날짜_입력하기() {
+
+    }
+
+    @Test
+    void 단답형_생성하기() {
+
+    }
+
+    @Test
+    void 장문형_생성하기() {
+
+    }
+
+    @Test
+    void 단답_옵션_생성하기() {
+
+    }
+
+    @Test
+    void 장문_옵션_생성하기() {
+
+    }
+
 }
