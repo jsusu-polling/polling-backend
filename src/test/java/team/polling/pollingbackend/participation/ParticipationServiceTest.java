ae578185a90507e66f7011f6c2898714c96e24b9
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 16:32:44 +0900

    test : ParticipationServiceTest 설정

--- src/test/java/team/polling/pollingbackend/participation/ParticipationServiceTest.java
+++ src/test/java/team/polling/pollingbackend/participation/ParticipationServiceTest.java
@@ -1,9 +1,31 @@
 package team.polling.pollingbackend.participation;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import team.polling.pollingbackend.participation.service.ParticipationService;
 
 public class ParticipationServiceTest {
 
     ParticipationService participationService;
+
+    @BeforeAll
+    void 데이터_삽입() {
+
+    }
+
+    @Test
+    void 설문조사_참여하기() {
+
+    }
+
+    @Test
+    void 설문조사_응답하기() {
+
+    }
+
+    @Test
+    void 설문조사_나가기() {
+
+    }
 
 }
