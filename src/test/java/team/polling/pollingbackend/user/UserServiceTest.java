d73b19a937c8d51b6837a93d86721e5fe2007218
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 17:05:56 +0900

    test : UserServiceTest 로직 생성

--- src/test/java/team/polling/pollingbackend/user/UserServiceTest.java
+++ src/test/java/team/polling/pollingbackend/user/UserServiceTest.java
@@ -1,4 +1,25 @@
 package team.polling.pollingbackend.user;
 
+import org.junit.jupiter.api.Test;
+import team.polling.pollingbackend.user.service.UserService;
+
 public class UserServiceTest {
+
+    UserService userService;
+
+    @Test
+    void 회원가입() {
+
+    }
+
+    @Test
+    void 로그인() {
+
+    }
+
+    @Test
+    void 회원정보_수정() {
+
+    }
+
 }
