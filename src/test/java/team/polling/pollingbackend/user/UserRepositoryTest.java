8e5fb885a3a74d5fa8f7b612c2eefe2898237c71
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 17:04:47 +0900

    test : UserRepositoryTest 로직 생성

--- src/test/java/team/polling/pollingbackend/user/UserRepositoryTest.java
+++ src/test/java/team/polling/pollingbackend/user/UserRepositoryTest.java
@@ -1,4 +1,29 @@
 package team.polling.pollingbackend.user;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import team.polling.pollingbackend.user.repository.UserRepository;
+
+@DataJpaTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 사용자_생성() {
+
+    }
+
+    @Test
+    void 사용자_이메일_조회() {
+
+    }
+
+    @Test
+    void 사용자_탈퇴() {
+
+    }
+
 }
