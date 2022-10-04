99ba6c774df6a5b8f86278dd319dee09829ffd73
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 17:00:19 +0900

    test : PaymentServiceTest 로직 생성

--- src/test/java/team/polling/pollingbackend/payment/PaymentServiceTest.java
+++ src/test/java/team/polling/pollingbackend/payment/PaymentServiceTest.java
@@ -1,4 +1,20 @@
 package team.polling.pollingbackend.payment;
 
+import org.junit.jupiter.api.Test;
+import team.polling.pollingbackend.payment.service.PaymentService;
+
 public class PaymentServiceTest {
+
+    private PaymentService paymentService;
+
+    @Test
+    void 지불_내역_가져오기() {
+
+    }
+
+    @Test
+    void 설문조사_보상_분배하기() {
+
+    }
+
 }
