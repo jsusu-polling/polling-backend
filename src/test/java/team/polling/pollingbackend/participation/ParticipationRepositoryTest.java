7b4f49d31bd7448d78b9870940479e649e6667e6
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 16:29:51 +0900

    test : ParticipationRepository 테스트 추가

--- src/test/java/team/polling/pollingbackend/participation/ParticipationRepositoryTest.java
+++ src/test/java/team/polling/pollingbackend/participation/ParticipationRepositoryTest.java
@@ -10,4 +10,30 @@ import team.polling.pollingbackend.participation.repository.ParticipationReposit
 @DataJpaTest
 public class ParticipationRepositoryTest {
 
+    @Autowired
+    private ParticipationRepository participationRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @BeforeEach
+    void 설문조사_참여하기() {
+
+    }
+
+    @Test
+    void 참여한_설문조사_전체_조회() {
+
+    }
+
+    @Test
+    void 참여한_특정_설문조사_조회() {
+
+    }
+
+    @Test
+    void 참여한_설문조사_내_특정_질문_조회() {
+
+    }
+    
 }
