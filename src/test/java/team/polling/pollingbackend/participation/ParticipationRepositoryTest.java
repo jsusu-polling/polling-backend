d6b9ea53a40dbd12988975a18d1adf7eaffad4e9
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 16:29:29 +0900

    test : 참여한 설문조사 테스트 환경 세팅

---
+++ src/test/java/team/polling/pollingbackend/participation/ParticipationRepositoryTest.java
@@ -0,0 +1,13 @@
+package team.polling.pollingbackend.participation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import team.polling.pollingbackend.participation.repository.AnswerRepository;
+import team.polling.pollingbackend.participation.repository.ParticipationRepository;
+
+@DataJpaTest
+public class ParticipationRepositoryTest {
+
+}
---
+++ src/test/java/team/polling/pollingbackend/participation/ParticipationServiceTest.java
@@ -0,0 +1,9 @@
+package team.polling.pollingbackend.participation;
+
+import team.polling.pollingbackend.participation.service.ParticipationService;
+
+public class ParticipationServiceTest {
+
+    ParticipationService participationService;
+
+}
