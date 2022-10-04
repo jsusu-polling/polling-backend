9d00172e2f5a11b34138a459da69af679130e55a
CEOJINSUNG <jinsung1048@gmail.com>
2022-10-04 16:47:09 +0900

    test : SurveyRepositoryTest 설정

--- src/test/java/team/polling/pollingbackend/survey/SurveyRepositoryTest.java
+++ src/test/java/team/polling/pollingbackend/survey/SurveyRepositoryTest.java
@@ -1,7 +1,65 @@
 package team.polling.pollingbackend.survey;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import team.polling.pollingbackend.survey.repository.QuestionRepository;
+import team.polling.pollingbackend.survey.repository.SelectionRepository;
+import team.polling.pollingbackend.survey.repository.SurveyRepository;
 
 @DataJpaTest
 public class SurveyRepositoryTest {
+
+    @Autowired
+    private SurveyRepository surveyRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private SelectionRepository selectionRepository;
+
+    @Test
+    @Order(1)
+    void 설문지_생성() {
+
+    }
+
+    @Test
+    @Order(2)
+    void 설문지_단답형_생성() {
+
+    }
+
+    @Test
+    @Order(3)
+    void 설문지_장문형_생성() {
+
+    }
+
+    @Test
+    @Order(4)
+    void 설문지_단답_옵션_생성() {
+
+    }
+
+    @Test
+    @Order(5)
+    void 설문지_장문_옵션_생성() {
+
+    }
+
+    @Test
+    @Order(6)
+    void 설문지_시작_마감_날짜_설정() {
+
+    }
+
+    @Test
+    @Order(7)
+    void 설문지_삭제() {
+
+    }
+
 }
